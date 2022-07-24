2a75c872828f1d42a244123d7021613db88ffe95
Saule <saulekazanbaeva3110@gmail.com>
2022-07-24 17:22:33 +0300

    update new post state

--- src/App.js
+++ src/App.js
@@ -11,30 +11,28 @@ function App() {
     { id: 3, title: "Title 3", body: "Description 3" },
   ]);
 
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const [post, setPost] = useState({ title: "", body: "" });
 
   const addNewPost = (e) => {
     e.preventDefault();
 
-    const newPost = { id: Date.now(), title, body };
+    const newPost = { id: Date.now(), title: post.title, body: post.body };
     setPosts([...posts, newPost]);
-    setTitle("");
-    setBody("");
+    setPost({ title: "", body: "" });
   };
 
   return (
     <div className="app">
       <form>
         <MyInput
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          value={post.title}
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
           type="text"
           placeholder="Название"
         />
         <MyInput
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
+          value={post.body}
+          onChange={(e) => setPost({ ...post, body: e.target.value })}
           type="text"
           placeholder="Описание"
         />
