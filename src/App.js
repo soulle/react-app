4f48a329a65cbadf387ec892dc0f3b3e6637436c
Saule <saulekazanbaeva3110@gmail.com>
2022-07-24 16:32:30 +0300

    add post item

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, { useState } from "react";
-import { ClassCounter } from "./components/ClassCounter";
-import { Counter } from "./components/Counter";
+import { PostItem } from "./components/PostItem";
+import "./styles/App.css";
 
 function App() {
   const [value, setValue] = useState("");
 
   return (
-    <div className="App">
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
-      />
-      <Counter />
-      <ClassCounter />
+    <div className="app">
+      <PostItem />
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const PostItem = () => {
+  return (
+    <div className="post">
+      <div className="post__content">
+        <strong>1. JavaScript</strong>
+        <div>Javascript - язык программирования</div>
+      </div>
+      <div className="post__btns">
+        <button>Удалить</button>
+      </div>
+    </div>
+  );
+};
