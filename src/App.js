75bce9346f26e91a391b283c44400b5bc627fec8
Saule <saulekazanbaeva3110@gmail.com>
2022-07-24 15:53:35 +0300

    add counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App">App</div>;
+  const [likes, setLikes] = useState(0);
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+
+  return (
+    <div className="App">
+      <h1>{likes}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 }
 
 export default App;
