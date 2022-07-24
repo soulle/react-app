477312cbe80283d2adae4b3fa4e587c42e854b41
Saule <saulekazanbaeva3110@gmail.com>
2022-07-24 17:13:45 +0300

    add examples

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { PostList } from "./components/PostList";
+import { MyInput } from "./components/UI/button/input/MyInput";
+import { MyButton } from "./components/UI/button/MyButton";
 import "./styles/App.css";
 
 function App() {
@@ -9,8 +11,27 @@ function App() {
     { id: 3, title: "Title 3", body: "Description 3" },
   ]);
 
+  const [title, setTitle] = useState("");
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log("title", title);
+    console.log("body", bodyInputRef.current.value);
+  };
+
   return (
     <div className="app">
+      <form>
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название"
+        />
+        <MyInput ref={bodyInputRef} type="text" placeholder="Описание" />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title="Список постов" />
     </div>
   );
