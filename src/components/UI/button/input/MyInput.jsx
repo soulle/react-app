04c6db25a632aee0ffefd52eec77bee0f1226848
Saule <saulekazanbaeva3110@gmail.com>
2022-07-24 17:13:29 +0300

    add input component

---
+++ src/components/UI/button/input/MyInput.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./MyInput.module.css";
+
+export const MyInput = React.forwardRef((props, ref) => {
+  return <input ref={ref} className={classes.myInput} {...props} />;
+});
